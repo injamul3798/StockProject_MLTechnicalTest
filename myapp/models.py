347d7f124b1d2a710e1a10d29ed728fe9df769fc
Injamul Haque <101572467+injamul3798@users.noreply.github.com>
2024-02-13 19:24:29 +0600

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+#This is for jsonModel
 class Trade(models.Model):
     date = models.DateField()
     trade_code = models.CharField(max_length=20)
@@ -9,7 +9,7 @@ class Trade(models.Model):
     close = models.FloatField()
     volume = models.IntegerField()
     
-    
+#If we want to use database then we have use this model and register in admin.py.    
 class DatabaseModel(models.Model):
     date = models.DateField()
     trade_code = models.CharField(max_length=20)
